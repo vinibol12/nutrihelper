83965e2031ee171fa97486d5d97c7814ff0386c3
Vinicius <vinibol12@yahoo.com.br>
2016-11-12 19:05:14 +1300

    Add main page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,8 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+//= require bootstrap.min
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
